c0c8643cfab1ecbe2ee0fb522069b23ee22d673a
P693 <panneerselvam.p@novactech.in>
2022-11-09 13:20:45 +0530

    0.0.1

---
+++ lib/cached_svg_image.dart
@@ -0,0 +1,114 @@
+library cached_svg_image;
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CachedNetworkSvgImage extends StatefulWidget {
+  const CachedNetworkSvgImage(
+      {Key? key,
+      required this.loadinggif,
+      required this.url,
+      this.width,
+      this.height,
+      this.fit = BoxFit.contain,
+      this.alignment = Alignment.center,
+      this.pictureProvider,
+      this.placeholderBuilder,
+      this.matchTextDirection = false,
+      this.allowDrawingOutsideViewBox = false,
+      this.semanticsLabel,
+      this.excludeFromSemantics = false,
+      this.clipBehavior = Clip.hardEdge,
+      this.cacheColorFilter = false,
+      this.colorFilter,
+      this.theme,
+      this.color,
+      this.colorBlendMode = BlendMode.srcIn})
+      : super(key: key);
+
+  final String loadinggif;
+  final String url;
+  final double? width;
+  final double? height;
+  final AlignmentGeometry alignment;
+  final PictureProvider? pictureProvider;
+  final WidgetBuilder? placeholderBuilder;
+  final bool allowDrawingOutsideViewBox;
+  final String? semanticsLabel;
+  final Clip clipBehavior;
+  final ColorFilter? colorFilter;
+  final bool cacheColorFilter;
+  final SvgTheme? theme;
+  final Color? color;
+  final BlendMode colorBlendMode;
+  final BoxFit fit;
+  final bool matchTextDirection;
+  final bool excludeFromSemantics;
+
+  @override
+  State<CachedNetworkSvgImage> createState() => _CachedNetworkSvgImageState();
+}
+
+class _CachedNetworkSvgImageState extends State<CachedNetworkSvgImage> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<File>(
+        future: getImageData(),
+        builder: ((context, snapshot) {
+          if (snapshot.data != null) {
+            File? file = snapshot.data;
+            return SvgPicture.file(
+              file!,
+              width: widget.width,
+              alignment: widget.alignment,
+              allowDrawingOutsideViewBox: widget.allowDrawingOutsideViewBox,
+              cacheColorFilter: widget.cacheColorFilter,
+              clipBehavior: widget.clipBehavior,
+              color: widget.color,
+              colorBlendMode: widget.colorBlendMode,
+              excludeFromSemantics: widget.excludeFromSemantics,
+              fit: widget.fit,
+              height: widget.height,
+              key: widget.key,
+              matchTextDirection: widget.matchTextDirection,
+              placeholderBuilder: widget.placeholderBuilder,
+              semanticsLabel: widget.semanticsLabel,
+              theme: widget.theme,
+            );
+          }
+          return SizedBox(
+            width: widget.width,
+            height: widget.height,
+            child: Center(
+              child: Image.asset(
+                widget.loadinggif,
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+              ),
+            ),
+          );
+        }));
+  }
+
+  Future<File> getImageData() async {
+    File file = await DefaultCacheManager().getSingleFile(widget.url);
+    return file;
+  }
+}
+
+class CachedNetworkSvgImageManageUtils {
+  static void removeFileCache(String url) {
+    DefaultCacheManager().removeFile(url).then((value) {
+      debugPrint('File removed');
+    }).onError((error, stackTrace) {
+      debugPrint(error.toString());
+    });
+  }
+
+  void clearCacheAll() {
+    DefaultCacheManager().emptyCache();
+  }
+}
