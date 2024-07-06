97d6a1480de100852b29e65d1fd8d9f7635331da
PS Developer's <panneer.developer@gmail.com>
2024-07-06 14:06:42 +0530

    # 0.0.7 * TODO: example updated.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,14 @@
-## 0.0.6
+# 0.0.7
+* TODO: example updated.
 
-* TODO: forceRefresh added
+# 0.0.6
+* TODO: forceRefresh added.
+
+# 0.0.5
+* TODO: update dependencies.
+
+# 0.0.4
+* TODO: issue fixed.
+
+# 0.0.3
+* TODO: initial release.
---
+++ example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:cached_svg_image/cached_svg_image.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cached SVG Image',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Cached SVG Image Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: CachedNetworkSvgImage(
+          url: "https://upload.wikimedia.org/wikipedia/commons/4/44/Google-flutter-logo.svg", loadingGif: '{your loading assets}',
+        ),
+      ),
+    );
+  }
+}
